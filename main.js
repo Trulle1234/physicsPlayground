8d6ef1cabc80bd52e412256ddb56ccf4cdf47f47
Trulle1234 <leonard.trulsson@gmail.com>
2025-07-08 12:25:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+const engine = Engine.create();
+
+const render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+const width = 1920;
+const height = 1080;
+const wallWidth = 15;
+
+Matter.Render.setSize(render, width, height);
+
+const ground = Bodies.rectangle(width / 2, height + (wallWidth / 2), width, wallWidth, { isStatic: true });
+const wallLeft = Bodies.rectangle(0 - (wallWidth / 2), height, wallWidth, height * 2, { isStatic: true });
+const wallRight = Bodies.rectangle(width + (wallWidth / 2), height, wallWidth, height * 2, { isStatic: true });
+const ceiling = Bodies.rectangle(width / 2, 0 - (wallWidth / 2), width, wallWidth, { isStatic: true, isSensor: true });
+
+let objects = [ground, wallLeft, wallRight, ceiling];
+let x = 50;
+let y = 200;
+const size = 80;
+const sides = 32;
+let sizeOffset = 0;
+const sizeOffsetStrength = 0.2;
+const Amount = 100;
+let placeBox = false;
+
+for (let i = 0; i < Amount; i++) {
+    if (placeBox) {
+    var box = Bodies.rectangle(x, y, size + sizeOffset, size + sizeOffset);
+    }
+    else {
+    var box = Bodies.polygon(x, y, sides, (size + sizeOffset)/ 2);
+    }
+    
+    sizeOffset = (Math.random() * (size * sizeOffsetStrength));
+
+    placeBox = !placeBox
+
+    objects[i + 4] = box
+    x += (size + size / 10)
+    y -= size / 5
+    
+    if (x > 750) {
+        x = 50
+        y += (size + size / 10)
+    }
+};
+
+Composite.add(engine.world, objects);
+
+Render.run(render);
+
+
+var runner = Runner.create();
+
+Runner.run(runner, engine);
